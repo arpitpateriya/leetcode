11a333089ed32a2c228618e430a33fe01b16e522
Arpit Pateriya <arpitpateriya14@gmail.com>
2025-08-05 23:04:04 +0530

    Time: 96 ms (97.57%), Space: 71.3 MB (19.4%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -11,7 +11,7 @@ class NumMatrix {
     for (int i = 0; i < m; ++i)
       for (int j = 0; j < n; ++j)
         prefix[i + 1][j + 1] = matrix[i][j] + prefix[i][j + 1] + prefix[i + 1][j] - prefix[i][j];
-  
+  }
 
   public int sumRegion(int row1, int col1, int row2, int col2) {
     return prefix[row2 + 1][col2 + 1] - prefix[row1][col2 + 1] //
