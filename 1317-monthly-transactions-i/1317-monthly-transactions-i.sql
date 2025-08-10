fda7a25e9a104afd7d5e6b045fadce9185dd5439
Arpit Pateriya <arpitpateriya14@gmail.com>
2025-08-10 21:55:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -7,4 +7,4 @@ SELECT
     SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
     SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
-GROUP BY month, country;
+GROUP BY month, countr
